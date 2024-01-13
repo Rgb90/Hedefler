5d293b035b31ed05526113bbe57574e676f7495f
Rgb90 <143276231+Rgb90@users.noreply.github.com>
2024-01-13 13:36:12 +0300

    add.edit

--- src/components/GoalList.jsx
+++ src/components/GoalList.jsx
@@ -1,57 +1,85 @@
 import { useState } from 'react';
-import { Container, Row, Col, } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form';
+import { Form } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+display: flex;
+align-items: end;
+gap: 24px;
+padding: 32px;
+`;
+
+const LiElement = styled.li`
+cursor: pointer;   /* mouse imleci tıklama şeklini alması için */
+list-style-type: disc;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #BF4F74;
+`;
+
+const Btn = styled.button`
+justify-content: space-between;
+  background: #BF4F74;
+  border-radius: 5px;
+  border: none;
+  color: white;
+  padding: 12px;
+`;
+
 
 export function GoalList() {
 
     const [todoInput, setTodoInput] = useState("");
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState(['Videoları izlemek', 'Proje yapmak']);
 
     return (
-        <>
-            <Container className='py-5'>
-                <ul>
-                    {todos.map((todo, index) => (
-                        <li key={index}>
-                            {todo.text}</li>)
-                    )}
-                </ul>
-
+        <div>
+            <Wrapper>
                 <Form>
-                    <Row>
-                        <Form.Group className="mb-3" controlId="formText">
-                            <Form.Label>Hedef</Form.Label>
-                            <Col xs={6}>
-                                <Form.Control className='d-flex' value={todoInput} onChange={setTodoInput} name="text" type="text" /> </Col>
-                            <Button onClick={addTodo} type="submit">Ekle</Button>
-                        </Form.Group>
-                    </Row>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Title>2024 Yılı Hedeflerim</Title>
+                        <Form.Control
+                            /* Input’umuzun “value” kısmını “todoInput” state’inden çekelim */
+                            value={todoInput}
+                            /* input’umuzun “onChange” metoduna bu state’in setTodoInput fonksiyonunu 
+                               input değiştikçe çalışacak şekilde koyalım  */
+                            onChange={(e) => { //inputtan gelen e değerini setTodoInput ile işledik.
+                                setTodoInput(e.target.value);
+                            }}
+                            /* Bizim inputa girdiğimiz herhangi bir değer setTodoInput 
+                            fonksiyonuyla todoInput' a gelecek  
+                            Bu kod inputun içine yazılmalıdır!!! */
+                            type="text"
+                        />
+                    </Form.Group>
                 </Form>
-            </Container >
-        </>
+                <Btn   /* Yeni bir fonksiyon oluşturalım. İsmi “addTodo” olsun
+                Bu fonksiyonu Ekle butonumuza tıklandığı zaman çalışacak şekilde ayarlayalım
+                Fonksiyonumuz çalıştığı zaman “todos” state’imizdeki array’e, “todoInput"state’indeki değeri ekleyelim */
+                    onClick={(addTodo) => {
+                        setTodos([...todos, todoInput]);
+                        setTodoInput(""); /* addTodo fonksiyonunun içerisinde setTodoInput fonksiyon çağrımını ekleyip boş
+                        string (””) alacak şekilde ayarlamalıyız. Bu sayede input’umuzun bağlı olduğu state’i temizleyeceğiz */
+                    }}
+                    /* variant='success' */>
+                    Ekle</Btn>
+            </Wrapper>
+            <ul>
+                {todos.map((todo, index) => (     /* // Listenin içindeki “li” elementlerini, todos array’inden “map” edelim. Key vermek gerekiyor */
+                    <LiElement onClick={() => {
+                        setTodos((oldValues) => {
+                            return oldValues.filter((oldTodo) => oldTodo !== todo);
+                        });  /* Liste elemanlarına onClick vereceğiz. Listedeki 
+                        herhangi bir hedefin üstüne tıklayınca o hedefi todos state’inden kaldırmış olacağız. */
+                    }}
+                        key={index}>{todo}
+                    </LiElement>
+                ))}
+            </ul>
+        </div>
     );
 };
-
-const addTodo = () => {
-    return (
-    <React.Fragment>
- {todos.map((todo, index) => {
-      <li key={todo.text}>
-        <input type="text" name="name" value={index.text} />
-      </li>
-    })}
-    </React.Fragment>
-)}
-
-/*  {
-    setTodos(oldTodos => [...oldTodos, `index ${oldTodos.length}`]);
-};
-
-return [
-    <input type="button" onClick={addTodo} value="Add" />,
-    <ul>{todos.map(index =>
-      <li>{index}</li>
-    )}
-    </ul>
-]; */
